da0191314d725c9829aec346a17d1eb111b51140
GavinLakner <gavinlakner@icloud.com>
2025-08-31 21:08:43 -0500

    Update MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -1,12 +1,18 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gavin Lakner
+#Date: 8/30/2025
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+verb1 = input("Enter a verb: ")
+noun1 = input("Enter a noun: ")
+adjective1 = input("Enter an adjective: ")
+noun2 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb2 = input("Enter a verb: ")
+noun3 = input("Enter a noun: ")
   #Print the story with the user supplied words.
 
 
