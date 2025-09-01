d5eff7c277035278a147b0bf5190350b0bb6d0da
GavinLakner <gavinlakner@icloud.com>
2025-08-31 21:37:30 -0500

    Update MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -14,9 +14,13 @@ adjective2 = input("Enter an adjective: ")
 verb2 = input("Enter a verb: ")
 noun3 = input("Enter a noun: ")
   #Print the story with the user supplied words.
-
-
-
+print("Once upon a time there was a man who liked to " + verb1 + ".")
+print("This man was always hanging out with friends at the local " + noun1 + ".")
+print("All of his peers noticed that he was always so " + ajective1 + ".")
+print("The man didn't care because he always had his " + noun2 + " to rely on.")
+print("His " + noun2 + " was super " + adjective2 + " and always there for him.")
+print("One day this man was " + verb2 + " to the " + noun1 + " when he got a strange call.")
+print("His " + noun3 + " got in trouble and he had to go rescue them from a dragon!")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
